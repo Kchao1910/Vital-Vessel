0d61aafbee411c70b173817400d8a2fe143a0bff
Kchao1910 <kchao1910@gmail.com>
2019-11-17 22:59:36 -0800

    Updates

---
+++ precache-manifest.37f144bc46cf3c35527d49c01df0df5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd718b65bf3d113916795a4f256c5c07",
+    "url": "/Vital-Vessel/index.html"
+  },
+  {
+    "revision": "d1b1708e86aacc74d1eb",
+    "url": "/Vital-Vessel/static/css/main.917d19a0.chunk.css"
+  },
+  {
+    "revision": "c305da58523e7f30d874",
+    "url": "/Vital-Vessel/static/js/2.34d8e5ff.chunk.js"
+  },
+  {
+    "revision": "d1b1708e86aacc74d1eb",
+    "url": "/Vital-Vessel/static/js/main.fdcb6923.chunk.js"
+  },
+  {
+    "revision": "41b27857d93f1fe94515",
+    "url": "/Vital-Vessel/static/js/runtime-main.f9b15199.js"
+  }
+]);
